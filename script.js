c782983a88d021e944591bb6b41c5146728bb917
Glauber Martins Murta <67845929+glaubermurta@users.noreply.github.com>
2021-11-22 08:33:10 -0300

    Adição dos arquivos

---
+++ script.js
@@ -0,0 +1,13 @@
+var hora = new Date().getHours()
+var saudacao
+
+
+if (hora < 12){
+    saudacao = "Bom dia!"
+}else if(hora > 18){
+    saudacao = "Boa Tarde!"
+}else{
+    saudacao = "Boa Noite!"
+}
+
+document.getElementById("mensagem").innerHTML = saudacao
